294bcf4e25873f758b29943a22990f7c6e6fb4e5
tobias <tobias.leong@zendesk.com>
2023-01-07 00:43:32 +0800

    Complete implementation of exercise-equivalent-binary-trees/ex.go

--- exercise-equivalent-binary-trees/ex.go
+++ exercise-equivalent-binary-trees/ex.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"golang.org/x/tour/tree" // imported from tour
 )
@@ -16,42 +17,73 @@ func Walk(t *tree.Tree, ch chan int) {
 	}
 }
 
+func goWalk(t *tree.Tree, ch chan int) {
+	defer close(ch)
+	Walk(t, ch)
+}
+
+func collectValues(vals *[]int, ch chan int) {
+	for v := range ch {
+		*vals = append(*vals, v)
+	}
+}
+
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
 	ch1 := make(chan int)
 	ch2 := make(chan int)
 
-	go Walk(t1, ch1)
-	go Walk(t2, ch2)
+	go goWalk(t1, ch1)
+	go goWalk(t2, ch2)
 
-	i := 0
+	vals1, vals2 := []int{}, []int{}
 
-	// not same length would mean
-	if len(ch1) != len(ch2) {
+	collectValues(&vals1, ch1)
+	collectValues(&vals2, ch2)
+
+	return EqualSlice(vals1, vals2)
+}
+
+func EqualSlice(s1, s2 []int) bool {
+	if len(s1) != len(s2) {
 		return false
 	}
 
-	for i < len(ch1) {
-		if <-ch1 != <-ch2 {
+	for i, v := range s1 {
+		if v != s2[i] {
 			return false
 		}
 	}
 	return true
 }
 
-func main() {
-	i, ch1, ch2 := 0, make(chan int), make(chan int)
-	t1, t2 := tree.New(1), tree.New(1)
-	go Walk(t1, ch1)
-	go Walk(t2, ch2)
-	for i < 10 {
-		fmt.Println("ch1:", <-ch1)
-		fmt.Println("ch2:", <-ch2)
-		i++
-	}
+func PrintTree(t *tree.Tree) string {
+	vals, ch := []int{}, make(chan int)
+	go goWalk(t, ch)
 
-	fmt.Println(Same(t1, t2)) // should be true
-	fmt.Println(Same(t1, tree.New(2)))
+	for v := range ch {
+		vals = append(vals, v)
+	}
+	return convertValsToString(vals)
+}
+
+func convertValsToString(val []int) string {
+	s := "Tree:"
+	for _, v := range val {
+		s += " " + strconv.Itoa(v)
+	}
+	return s
+}
+
+func main() {
+	t1, t1b, t2, t3 := tree.New(1), tree.New(1), tree.New(2), tree.New(3)
+
+	fmt.Println(PrintTree(t1))
+	fmt.Println(PrintTree(t2))
+	fmt.Println(PrintTree(t3))
+
+	fmt.Println(Same(t1, t1b)) // should be true
+	fmt.Println(Same(t1, t2))  // should be false
 
 }
